13791de3419ba1667f5f00324ac578dc510acc39
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-10-11 09:51:04 -0500

    saliendo de module

--- backend/helpers/handleStorage.js
+++ backend/helpers/handleStorage.js
@@ -1,26 +1,13 @@
 const multer = require("multer");
-const { fileURLToPath } = require("url");
-const { dirname } = require("path");
-
-const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
-const MIMETYPES = ["image/jpeg", "image/png"];
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const pathStorage = `${CURRENT_DIR}/../uploads`;
+  destination: function (req, file, cb) {
+    const pathStorage = `${__dirname}/../storage`;
     cb(null, pathStorage);
   },
-  filename: (req, file, cb) => {
+  filename: function (req, file, cb) {
     const ext = file.originalname.split(".").pop();
     const filename = `file-${Date.now()}.${ext}`;
     cb(null, filename);
-  },
-
-  fileFilter: (req, file, cb)=> {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-      cb(null, true);
-    } else {
-      cb(new Error('Formato invalido'));
-    }
   },
 });
 
--- backend/index.js
+++ backend/index.js
@@ -8,21 +8,21 @@ const port = process.env.PORT || 3000;
 dbconnect();
 
 // Configurar CORS
-// const whitelist = [process.env.FRONTEND_URL];
+const whitelist = [process.env.FRONTEND_URL];
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.includes(origin)) {
-//       // Puede consultar la API
-//       callback(null, true);
-//     } else {
-//       // No esta permitido
-//       callback(new Error("Error de Cors"));
-//     }
-//   },
-// };
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      // Puede consultar la API
+      callback(null, true);
+    } else {
+      // No esta permitido
+      callback(new Error("Error de Cors"));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 if (NODE_ENV !== "test") {
   app.listen(port, (req, res) => {
