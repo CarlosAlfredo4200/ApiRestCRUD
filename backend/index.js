695e2f381f8ac677da92b5379778886374d33d01
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-10-11 10:19:55 -0500

    cambio mac

--- backend/index.js
+++ backend/index.js
@@ -3,30 +3,30 @@ require('dotenv').config()
 const cors = require("cors");
 const dbconnect = require("./config/db.js");
 
-const NODE_ENV = process.env.NODE_ENV || "development";
+
 const port = process.env.PORT || 3000;
 dbconnect();
 
-// Configurar CORS
-const whitelist = [process.env.FRONTEND_URL];
+// // Configurar CORS
+// const whitelist = [process.env.FRONTEND_URL];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      // Puede consultar la API
-      callback(null, true);
-    } else {
-      // No esta permitido
-      callback(new Error("Error de Cors"));
-    }
-  },
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.includes(origin)) {
+//       // Puede consultar la API
+//       callback(null, true);
+//     } else {
+//       // No esta permitido
+//       callback(new Error("Error de Cors"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-if (NODE_ENV !== "test") {
-  app.listen(port, (req, res) => {
-    console.log(`Escuchando en el puerto ${port}`);
-  });
-}
- 
+
+app.listen(port, (req, res) => {
+  console.log(`Escuchando en el puerto ${port}`);
+});
+
+
